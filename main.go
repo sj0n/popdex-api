4ccabbc78d3495236ab237843f058581a93f025e
sj0n <hariz@proton.me>
2025-01-31 22:20:51 +0800

    add: cors

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	
+	"github.com/labstack/echo/v4/middleware"
+
 	"github.com/sj0n/echo-api/pkg/routes"
 )
 
 func main() {
 	server := echo.New()
+
+	server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost", "https://popdex.pages.dev", "https://popdex.harizdan.xyz"},
+	}))
 
 	server.GET("/pokemon/:name", routes.GetPokemonProfile)
 	server.GET("/pokemon/:name/moves", routes.GetPokemonMoves)
